84e1396e34ec0618898050b38f30740a236ce7cf
KulashekarThurlu <thurlukulasekhar@gmail.com>
2023-08-05 11:59:31 +0530

    remove spaces

--- TimeTrackBackend/src/main/java/com/app/timetrack/TimeTrackBackendApplication.java
+++ TimeTrackBackend/src/main/java/com/app/timetrack/TimeTrackBackendApplication.java
@@ -24,7 +24,7 @@ public class TimeTrackBackendApplication {
 		        // Printing the product array
 		        for (int i = 0; i < result.length; i++) {
 		            System.out.print(result[i] + " ");
-		        }
+		        }  
 		    
 
 		
